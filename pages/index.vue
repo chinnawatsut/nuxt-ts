1653df48da1c0e0368e223490f7233acea086094
Chinnawat K <chinnawatsut@gmail.com>
2018-11-04 23:14:39 +0700

    add example

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,56 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        store-front
-      </h1>
-      <h2 class="subtitle">
-        My brilliant Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <h1>Hello, This Is Nuxt + TypeScript</h1>
+    <h2>Look on your console</h2>
+    {{ count }}
+    <button @click="addToCount(1)">Add</button>
+    <button @click="resetCount">Reset</button>
+  </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'nuxt-class-component'
+import {
+  State,
+  Getter,
+  Action,
+  Mutation,
+  namespace
+} from 'vuex-class'
 
-export default {
-  components: {
-    Logo
+@Component
+export default class Index extends Vue {
+  count = 0
+  msg = 'Hello, Prayuth'
+
+  fetch () {
+    console.log('fetch')
+  }
+
+  async asyncData () {
+    console.log('asyncData called')
+  }
+
+  created() {
+    console.log('created')  
+  }
+
+  mounted () {
+    this.greet()
+  }
+
+  addToCount(n: number) {
+    this.count += n
+  }
+
+  resetCount() {
+    this.count = 0
+  }
+
+  greet () {
+    console.log('greeting: ' + this.msg)
   }
 }
+
 </script>
-
-<style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
